882f97e27e12809bc4045d428d1b55377df7f1df
DevGreick <jackson.greick@gmail.com>
2018-05-30 22:15:11 -0300

    Questionario 4
    Signed-off-by: DevGreick <jackson.greick@gmail.com>

---
+++ SomaCorreta.py
@@ -0,0 +1,7 @@
+#questionario 4 soma correta entre os dois numeros
+numero1 = int(input('digite seu primeiro numero'))
+numero2 = int(input('digite seu segundo numero'))
+soma = numero1 + numero2
+print('a soma de {} com {} e igual a {}'.format(numero1, numero2, soma,))
+
+
