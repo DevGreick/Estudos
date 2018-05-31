86950b229fb100ca98d8dd753d7ce6d7385f43cd
DevGreick <jackson.greick@gmail.com>
2018-05-30 23:56:26 -0300

    Exercicio com questionario 4 aula 6
    Signed-off-by: DevGreick <jackson.greick@gmail.com>

--- exercicio4aula6.py
+++ exercicio4aula6.py
@@ -1,7 +1,7 @@
 #Digitar algo e que apareca o formato e seus atributos
-algo = input ('digite algo para comparar')
+algo = input('digite algo para comparar')
 print(type('o valor e {}'.format(algo)))
-print(algo.isalnum())
-print(algo.isdecimal())
-print(algo.islower())
-print(algo.isupper())
+print('e alfanumerico',algo.isalnum())
+print('e decimal',algo.isdecimal())
+print('e minusculas',algo.islower())
+print('e maiusculas',algo.isupper())
