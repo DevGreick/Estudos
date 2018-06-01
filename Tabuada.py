8ecdf12b8b61ca17d80e8bc90fbe6c99c13079d5
JacksonGreick <jackson.greick@gmail.com>
2018-06-01 05:02:13 -0300

    Tabuada

---
+++ Tabuada.py
@@ -0,0 +1,25 @@
+# Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada
+numero = int(input('Digite o numero aqui'))
+m1 = numero * 1
+m2 = numero * 2
+m3 = numero * 3
+m4 = numero * 4
+m5 = numero * 5
+m6 = numero * 6
+m7 = numero * 7
+m8 = numero * 8
+m9 = numero * 9
+m10 = numero * 10
+print(numero, ' x 1 = {}'.format(m1))
+print(numero, 'x 2 = {}'.format(m2))
+print(numero, 'x 3 = {}'.format(m3))
+print(numero, 'x 4 = {}'.format(m4))
+print(numero, 'x 5 = {}'.format(m5))
+print(numero, 'x 6 = {}'.format(m6))
+print(numero, 'x 7 = {}'.format(m7))
+print(numero, 'x 8 = {}'.format(m8))
+print(numero, 'x 9 = {}'.format(m9))
+print(numero, 'x 10 = {}'.format(m10))
+
+
+
