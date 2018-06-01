108e57643493f02904c65c0c1567ce11b51b1e07
JacksonGreick <jackson.greick@gmail.com>
2018-06-01 04:15:52 -0300

    Soma de duas notas,calcule e  mostre sua media.

---
+++ Notas.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua media
+
+nota1 = float(input(' digite a primeira nota'))
+nota2 = float(input(' digite a segunda nota'))
+media = (nota1 + nota2) / 2
+print('a media é {}' .format(media))
+
+
+
