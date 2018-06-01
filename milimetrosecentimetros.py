07dcf20d295d43424dcf40a3bf5f56ff9ac5a303
JacksonGreick <jackson.greick@gmail.com>
2018-06-01 04:40:44 -0300

    Converter metro para milimetro e metro pra centimetro

---
+++ milimetrosecentimetros.py
@@ -0,0 +1,5 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+numero = float(input('digite o valor aqui'))
+centimetro = numero * 100
+milimetro = numero * 1000
+print('valor convertido em centimetro e {},valor convertido em milimetro e {}'.format (centimetro, milimetro))
