74cb160de875d4438a21b0b4d97098cb0ab28660
JacksonGreick <jackson.greick@gmail.com>
2018-06-01 04:06:09 -0300

    Dobro o triplo e a raiz

---
+++ exercicio006.py
@@ -0,0 +1,6 @@
+numero1 = int(input('digite o numero aqui'))
+dobro = numero1 * 2
+triplo = numero1 * 3
+raiz = numero1 ** (1/2)
+
+print('o dobro e {},o triplo e {} a raiz e {}'.format(dobro, triplo, raiz))
