5b885fddc3d380e431320ab970eb550b5b913ee4
JacksonGreick <jackson.greick@gmail.com>
2018-06-01 03:54:30 -0300

    Primitivos

---
+++ Primitivos.py
@@ -0,0 +1,14 @@
+# Saber as operações em python
+numero1 = int(input('digite seu primeiro numero aqui'))
+numero2 = int(input('digite seu segundo numero aqui'))
+soma = numero1 + numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+subtracao = numero1 - numero2
+potencia = numero1 ** numero2
+divisaoint = numero1 // numero2
+print('a soma é {}, a multiplicacao é {}, a divisao e {}, a subtracao e {}'.format(soma, multiplicacao, divisao, subtracao),end=' >>> ')
+print('a divisao inteira e {},a potencia e {}'.format(divisaoint, potencia))
+
+
+
