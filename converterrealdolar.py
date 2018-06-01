aba17cb94daf1aeb7f21913dd3d9a5c4fd8994dc
JacksonGreick <jackson.greick@gmail.com>
2018-06-01 05:12:09 -0300

    Real em dolar a 3.27

---
+++ converterrealdolar.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar sendo o dolar 3.27
+
+real = float(input('digite seu dinheiro aqui'))
+conversao = real / 3.27
+print('O real convertido fica {}'.format(conversao))
+
+
