c16bbdffdca73fb94825bf8154540122a217e6b7
JacksonGreick <jackson.greick@gmail.com>
2018-06-05 02:40:00 -0300

    Desafio 16

--- numeroreal.py
+++ numeroreal.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia um numero real qualquer pelo teclado e mostra na tela a sua porçao inteira
+# Desafio 16 Crie um programa que leia um numero real qualquer pelo teclado e mostra na tela a sua porçao inteira
 import math
 numero = float(input('Digite um numero qualquer'))
 print('o numero inteiro de {} e = a {} '.format(numero, math.floor(numero)))
