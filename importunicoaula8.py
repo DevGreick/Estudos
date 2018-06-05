2e8cacf5878e923acaf44b4b88c0a68079c61c11
JacksonGreick <jackson.greick@gmail.com>
2018-06-05 04:30:50 -0300

    import unico

--- importunicoaula8.py
+++ importunicoaula8.py
@@ -1,5 +1,6 @@
 from math import sqrt, floor
 numero = int(input('digite seu numero aqui'))
 raiz = sqrt(numero)
-print('A raiz de {} e = a {}'.format(numero, floor(raiz))
+print('A raiz de {} e = a {}'.format(numero, floor(raiz)))
+
 
