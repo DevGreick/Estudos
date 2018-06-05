3f4f02cc5eb3ce292f591c8c175d5a3e20132f62
JacksonGreick <jackson.greick@gmail.com>
2018-06-05 04:24:51 -0300

    Desafio 19

---
+++ desafio19.py
@@ -0,0 +1,18 @@
+# Escolher entre 4 biscoitos
+
+import random
+
+biscoito1 = str('maizena')
+biscoito2 = str('negrito')
+biscoito3 = str('negresco')
+biscoito4 = str('passatempo')
+lista = [biscoito1, biscoito2, biscoito3, biscoito4]
+aleatorio = random.choice(lista)
+print('o biscoito escolhido foi o {}'.format(aleatorio))
+
+
+
+
+
+
+
