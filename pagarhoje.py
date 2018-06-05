27c5c3b56a6a6661e6295a8cf8f6267e471f79ca
JacksonGreick <jackson.greick@gmail.com>
2018-06-05 06:17:51 -0300

    lanche

---
+++ pagarhoje.py
@@ -0,0 +1,14 @@
+import random,os
+n1 = str('Sandes')
+n2 = str('Jackson')
+n3 = str('Alexandro')
+n4 = str('Tião')
+Lista = [n1, n2, n3, n4]
+Aleatorio = random.choice(Lista)
+print('quem vai pagar o lanche hoje vai ser o {}'.format(Aleatorio))
+os.system('pause')
+
+
+
+
+
