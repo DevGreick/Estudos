f8d3a6a314c664df5a8994f5bd4e3f957f376b4b
DevGreick <jackson.greick@gmail.com>
2018-06-06 04:52:00 -0300

    teste
    Signed-off-by: DevGreick <jackson.greick@gmail.com>

--- milimetrosecentimetros.py
+++ milimetrosecentimetros.py
@@ -1,5 +1,5 @@
-#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
 numero = float(input('digite o valor aqui'))
 centimetro = numero * 100
 milimetro = numero * 1000
-print('valor convertido em centimetro e {},valor convertido em milimetro e {}'.format (centimetro, milimetro))
+print('valor convertido em centimetro e {},valor convertido em milimetro e {}'.format(centimetro, milimetro))
