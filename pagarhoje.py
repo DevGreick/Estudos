875bad2d8542196f93f13e2b779dd84021b69019
DevGreick <jackson.greick@gmail.com>
2018-06-06 04:25:10 -0300

    pagar hoje
    Signed-off-by: DevGreick <jackson.greick@gmail.com>

--- pagarhoje.py
+++ pagarhoje.py
@@ -6,8 +6,7 @@ n4 = str('Tião')
 Lista = [n1, n2, n3, n4]
 Aleatorio = random.choice(Lista)
 print('quem vai pagar o lanche hoje vai ser o {}'.format(Aleatorio))
-os.system('pause')
-
+os.system('read')
 
 
 
